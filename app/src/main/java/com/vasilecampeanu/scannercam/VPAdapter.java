6f6e10cd95133accc23320f07dca9a6d00e21692
vasilecampeanu <vasilecampeanu.link@yahoo.com>
2022-04-12 11:49:46 +0300

    feat[activity]: Add tab layout to welcome page activity

---
+++ app/src/main/java/com/vasilecampeanu/scannercam/VPAdapter.java
@@ -0,0 +1,41 @@
+package com.vasilecampeanu.scannercam;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+import androidx.viewpager2.adapter.FragmentStateAdapter;
+
+import java.util.ArrayList;
+
+public class VPAdapter extends FragmentStateAdapter {
+
+    private String[] titles = new String[] { "WPAGE01", "WPAGE02", "WPAGE03" };
+    
+    public VPAdapter(@NonNull FragmentActivity fragmentActivity) {
+        super(fragmentActivity);
+    }
+
+    @NonNull
+    @Override
+    public Fragment createFragment(int position) {
+        switch (position)
+        {
+            case 0:
+                return new WPAGE01();
+            case 1:
+                return new WPAGE02();
+            case 2:
+                return new WPAGE03();
+        }
+        
+        return new WPAGE01();
+    }
+
+    @Override
+    public int getItemCount() {
+        return titles.length;
+    }
+}
--- app/src/main/java/com/vasilecampeanu/scannercam/WelcomePage.java
+++ app/src/main/java/com/vasilecampeanu/scannercam/WelcomePage.java
@@ -1,16 +1,49 @@
 package com.vasilecampeanu.scannercam;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentPagerAdapter;
+import androidx.viewpager.widget.ViewPager;
+import androidx.viewpager2.widget.ViewPager2;
 
 import android.os.Bundle;
+import android.view.View;
+
+import com.google.android.material.tabs.TabLayout;
+import com.google.android.material.tabs.TabLayoutMediator;
 
 public class WelcomePage extends AppCompatActivity 
 {
 
+    private TabLayout  tabLayout;
+    private ViewPager2 viewPager;
+    
+    VPAdapter vpAdapter;
+
+    private String[] titles = new String[] { "WPAGE01", "WPAGE02", "WPAGE03" };
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) 
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome_page);
+
+        addWelcomeFragemnts();
+    }
+    
+    public void addWelcomeFragemnts()
+    {
+        tabLayout = findViewById(R.id.welcomeTabLayout);
+        viewPager = findViewById(R.id.welcomeViewPager);
+
+        vpAdapter = new VPAdapter(this);
+        viewPager.setAdapter(vpAdapter);
+        
+        new TabLayoutMediator(tabLayout, viewPager, ((tab, position) -> tab.setText(titles[position]))).attach();
+    }
+
+    public void onClickBtnCloseWelcomeActivity(View view) 
+    {
+        this.finish();
     }
 }
