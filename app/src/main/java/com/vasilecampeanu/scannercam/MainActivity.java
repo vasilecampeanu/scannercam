98c15ffdbccfb0d74e6298709b10a35b010b3c2d
vasilecampeanu <vasilecampeanu.link@yahoo.com>
2022-04-12 18:41:06 +0300

    feat: Ask for permissions

--- app/src/main/java/com/vasilecampeanu/scannercam/MainActivity.java
+++ app/src/main/java/com/vasilecampeanu/scannercam/MainActivity.java
@@ -1,13 +1,27 @@
 package com.vasilecampeanu.scannercam;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.viewpager2.widget.ViewPager2;
 
+import android.Manifest;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
 
 import com.google.android.material.tabs.TabLayout;
 import com.google.android.material.tabs.TabLayoutMediator;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity 
 {
@@ -16,12 +30,17 @@ public class MainActivity extends AppCompatActivity
     private MainVPAdapter mainVpAdapter;
 
     private String[] titlesMain = new String[] { "Home", "Docs", "Tools", "Settings"};
+
+    public static final int REQUEST_ID_MULTIPLE_PERMISSIONS= 7;
     
     @Override
     protected void onCreate(Bundle savedInstanceState) 
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Ask permsions
+        checkAndroidVersion();
         
         // Add main fragments
         addMainFragemnts();
@@ -30,7 +49,128 @@ public class MainActivity extends AppCompatActivity
         // Recomandat ar fi ca utilizatorul sa poata alege daca vrea sa vada sau nu aplicatia la urmatoarea deschidere a aplicatiei.
          openWelcomePage();
     
-        // Add fragments to tab layout
+        // Ask permsions
+    }
+
+    private void checkAndroidVersion()
+    {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M)
+        {
+            checkAndRequestPermissions();
+        }
+        else
+        {
+            // code for lollipop and pre-lollipop devices
+        }
+    }
+
+    private boolean checkAndRequestPermissions()
+    {
+        int camera = ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA);
+        int wtite = ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE);
+        int read = ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE);
+        List<String> listPermissionsNeeded = new ArrayList<>();
+
+        if (wtite != PackageManager.PERMISSION_GRANTED)
+        {
+            listPermissionsNeeded.add(Manifest.permission.WRITE_EXTERNAL_STORAGE);
+        }
+
+        if (camera != PackageManager.PERMISSION_GRANTED)
+        {
+            listPermissionsNeeded.add(Manifest.permission.CAMERA);
+        }
+
+        if (read != PackageManager.PERMISSION_GRANTED)
+        {
+            listPermissionsNeeded.add(Manifest.permission.READ_EXTERNAL_STORAGE);
+        }
+
+        if (!listPermissionsNeeded.isEmpty())
+        {
+            ActivityCompat.requestPermissions(this, listPermissionsNeeded.toArray(new String[listPermissionsNeeded.size()]), REQUEST_ID_MULTIPLE_PERMISSIONS);
+            return false;
+        }
+
+        return true;
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults)
+    {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        Log.d("in fragment on request", "Permission callback called-------");
+        switch (requestCode)
+        {
+            case REQUEST_ID_MULTIPLE_PERMISSIONS:
+            {
+
+                Map<String, Integer> perms = new HashMap<>();
+
+                // Initialize the map with both permissions
+                perms.put(Manifest.permission.WRITE_EXTERNAL_STORAGE, PackageManager.PERMISSION_GRANTED);
+                perms.put(Manifest.permission.CAMERA, PackageManager.PERMISSION_GRANTED);
+                perms.put(Manifest.permission.READ_EXTERNAL_STORAGE, PackageManager.PERMISSION_GRANTED);
+
+                // Fill with actual results from user
+                if (grantResults.length > 0)
+                {
+                    for (int i = 0; i < permissions.length; i++) perms.put(permissions[i], grantResults[i]);
+                    // Check for both permissions
+                    if (perms.get(Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED && perms.get(Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED && perms.get(Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED)
+                    {
+                        Log.d("in fragment on request", "CAMERA & WRITE_EXTERNAL_STORAGE READ_EXTERNAL_STORAGE permission granted");
+                        // process the normal flow
+                        //else any one or both the permissions are not granted
+                    }
+                    else
+                    {
+                        Log.d("in fragment on request", "Some permissions are not granted ask again ");
+
+                        // Permission is denied (this is the first time, when "never ask again" is not checked) so ask again explaining the usage of permission
+                        // ShouldShowRequestPermissionRationale will return true
+                        // Show the dialog or snackbar saying its necessary and try again otherwise proceed with setup.
+
+                        if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) || ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.CAMERA) || ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.READ_EXTERNAL_STORAGE)) {
+                            showDialogOK("Camera and Storage Permission required for this app",
+                                new DialogInterface.OnClickListener()
+                                {
+                                    @Override
+                                    public void onClick(DialogInterface dialog, int which)
+                                    {
+                                        switch (which)
+                                        {
+                                            case DialogInterface.BUTTON_POSITIVE:
+                                                checkAndRequestPermissions();
+                                                break;
+                                            case DialogInterface.BUTTON_NEGATIVE:
+                                                // Proceed with logic by disabling the related features or quit the app.
+                                                break;
+                                        }
+                                    }
+                                });
+                        }
+
+                        // Permission is denied (and never ask again is checked)
+                        // ShouldShowRequestPermissionRationale will return false
+                        else
+                        {
+                            Toast.makeText(this, "Go to settings and enable permissions", Toast.LENGTH_LONG).show();
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    private void showDialogOK(String message, DialogInterface.OnClickListener okListener)
+    {
+        new AlertDialog.Builder(this)
+            .setMessage(message)
+            .setPositiveButton("OK", okListener)
+            .setNegativeButton("Cancel", okListener)
+            .create()
+            .show();
     }
     
     // Deschidem pagina de pornire
