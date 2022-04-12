356ac9f4035f1a9c2f086c620128d5f98dc8a1ee
vasilecampeanu <vasilecampeanu.link@yahoo.com>
2022-04-12 10:01:27 +0300

    feat[revision]: Initial revision

---
+++ app/src/main/java/com/vasilecampeanu/scannercam/MainActivity.java
@@ -0,0 +1,14 @@
+package com.vasilecampeanu.scannercam;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
